8e9a08982027d5cd6f0cc4c7c04f184581fe8e28
Paulo Cunha <cunhapaulo4741@gmail.com>
2023-05-29 10:09:10 -0300

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [search, setSearch] = useState("");
@@ -11,7 +13,6 @@ function App() {
     const movieFilter = moviesList.filter((movie) =>
       movie.Title.toLowerCase().includes(search.toLowerCase())
     );
-    console.log(movieFilter);
 
     const movieData =
       movieFilter.length > 0
@@ -37,24 +38,50 @@ function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <input type="submit" value="Search Movie" />
-      </form>
-      {errorMessage && <span className="error">{errorMessage}</span>}
-      {moviesList.length > 0 &&
-        moviesList.map((movie) => (
-          <div key={movie.imdbID} className="card">
-            <h3>{movie.Title}</h3>
-            <p>
-              {movie.Year} {movie.Type}
-            </p>
-          </div>
-        ))}
+      <div className="search-container">
+        <div className="search-box">
+          <h1>Movie Search</h1>
+          <form onSubmit={handleSubmit} className="search-area">
+            <input
+              type="text"
+              value={search}
+              id="movie"
+              placeholder="Enter the movie or series name..."
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type="submit" value="Search Movie" id="search">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+            {errorMessage && <span className="error">{errorMessage}</span>}
+          </form>
+        </div>
+        <p>
+          Foto de{" "}
+          <a href="https://unsplash.com/@somethingmagical?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            something magical
+          </a>{" "}
+          na{" "}
+          <a href="https://unsplash.com/pt-br/fotografias/SdjA-_Xzuxg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </p>
+      </div>
+      <div className="info-container">
+        <div id="result">
+          {moviesList.length > 0 &&
+            moviesList.map((movie) => (
+              <div key={movie.imdbID} className="card">
+                <div>
+                  <h3>{movie.Title}</h3>
+                  <p>
+                    {movie.Year} {movie.Type}
+                  </p>
+                </div>
+                <img src={movie.Poster} alt="movie image" className="movie-poster"/>
+              </div>
+            ))}
+        </div>
+      </div>
     </>
   );
 }
